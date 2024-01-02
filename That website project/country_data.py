9fda2b011df796d3e683e25baf1699bcadca3815
LurinsRaivis <98687452+LurinsRaivis@users.noreply.github.com>
2024-01-02 11:49:52 +0200

    ch

--- That website project/country_data.py
+++
@@ -1,9 +0,0 @@
-countries_neighbors = {
-    "Germany": ["France", "Poland", "Netherlands", "Belgium", "Luxembourg", "Denmark", "Austria", "Czech Republic"],
-    "France": ["Spain", "Belgium", "Luxembourg", "Germany", "Switzerland", "Italy"],
-    "Italy": ["France", "Switzerland", "Austria", "Slovenia"],
-    "Spain": ["Portugal", "France"],
-    "Portugal": ["Spain"],
-    "Austria": ["Germany", "Italy", "Switzerland", "Liechtenstein", "Slovenia", "Czech Republic", "Slovakia", "Hungary"],
-    # Add other countries and their neighbors here
-}
